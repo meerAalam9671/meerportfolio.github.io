13842591b7421ecd05191a19a09ce3bf921d99df
Meer Aalam <142563973+meerAalam9671@users.noreply.github.com>
2023-12-17 09:17:01 -0500

    Add files via upload
    commit by meer

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ window.onscroll = ()=>{
     menuIcon.classList.remove('bx-x');
     navList.classList.remove('open');
 }
+
+const circles = document.querySelectorAll(".circle");
+circles.forEach(elem=>{
+    var dots = elem.getAttribute("data-dots");
+    var marked = elem.getAttribute("data-parcent");
+    var parcent = Math.floor(dots*marked/100);
+    var points = "";
+    var rotate = 360 / dots;
+
+    for(i=0; i < dots; i++){
+        points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+    }
+    elem.innerHTML = points;
+
+
+    const pointsmarked = document.querySelectorAll('.points');
+    for(let i = 0; i< parcent; i++){
+        pointsmarked[i].classList.add('marked');
+    } 
+})
